4db289385c0ee0d593dbf2dfa60825d0a978f6c2
Lee <duyphucle.0510@gmail.com>
2025-09-17 15:14:47 +0700

    modified server.js

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`🚀 Server is running at http://localhost:${PORT}`);
+    console.log(`🚀 Server is running `);
 });
